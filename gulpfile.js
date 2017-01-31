44c13e764bf952c9c129328f48df64f20023d060
Husain Alshehhi <husainaloos@gmail.com>
2017-01-30 18:12:29 -0600

    Adding nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('lint', function() {
     return gulp.src(['*.js', './routes/*.js', './models/*.js', './config/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
+});
+
+gulp.task('nodemon', function() {
+    var stream = nodemon({
+        script: 'start.js',
+        ext: 'html js',
+        verbose: true,
+        tasks: ['lint']
+    });
+
+    stream
+        .on('restart', function() {
+            console.log('restarted!')
+        })
+        .on('crash', function() {
+            console.error('Application has crashed!\n');
+            stream.emit('restart', 10);
+        });
 });
